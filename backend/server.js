337711fd382e2b06a3afaaecd755698bfc7445fc
Druvitha <druvitha.g@gmail.com>
2025-07-29 18:44:59 +1000

    Implement CRUD in taskController and update configs

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
@@ -6,21 +5,21 @@ const connectDB = require('./config/db');
 
 dotenv.config();
 
-
 const app = express();
 
+// Middleware
 app.use(cors());
 app.use(express.json());
+
+// API Routes
 app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/tasks', require('./routes/taskRoutes'));
 
-// Export the app object for testing
+// Start the server if this is the main file being run
 if (require.main === module) {
-    connectDB();
-    // If the file is run directly, start the server
+    connectDB(); // Establish the database connection
     const PORT = process.env.PORT || 5001;
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-  }
+}
 
-
-module.exports = app
+module.exports = app;
